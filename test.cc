f8ee171bd583e52a3104f36247ef94032b5cfa76
jacklong1953 <17855822406@sina.cn>
2019-05-18 20:14:50 +0800

    first  modify

---
+++ test.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+using std::cout;
+using std::endl;
+
+int main()
+{
+    cout << "the first sentence" << endl;
+    cout << "the second sentence" << endl;
+    cout << "the third sentence" << endl;
+    return 0;
+
+}
